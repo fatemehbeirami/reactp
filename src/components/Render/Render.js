03af60abe5cc6af8a5cb14f58244931de04b3ea8
fatemehbeirami <fatemehbeirami100@gmail.com>
2020-12-29 00:27:17 +0330

    my first commit

---
+++ src/components/Render/Render.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class Render extends Component {
+state = {
+    buy: [] , 
+    sell: []
+}
+    componentDidMount() {
+        console.log('------------------');
+        console.log(this.props.currency);
+        let fetchBuy = [] ; 
+        for(let i = 0 ; i < 15 ; i++){
+            fetchBuy.push({
+                ...this.props.currency[i] , 
+                id : i
+            });
+        }
+        let fetchSell = [] ; 
+        for(let j = 16 ; j < 30 ; j++){
+            fetchSell.push({
+                ...this.props.currency[j] , 
+                id : j
+            });
+        }
+        this.setState({
+            buy : fetchBuy , 
+            sell : fetchSell
+        });
+    }
+    render() {
+        return (
+            <div>
+                <p class="pname">beiramii</p>
+                {this.props.currency.map((currency) => {
+                    return (
+                        <div  key={currency.id} style = {{border : '1px solid black'}}>
+                            <p class="info-style">{currency.slug}</p>
+                            <p>{currency.p}</p>
+                        </div>
+                    )
+                })}
+                <p>-----------------------------</p>
+                 {this.state.buy.map((buy) => {
+                    return (
+                        <div key={buy.id} style = {{border : '1px solid black'}}>
+                            <p>{buy.slug}</p>
+                            <p>{buy.p}</p>
+                        </div>
+                    )
+                })}
+                 <p>-----------------------------</p>
+                 {this.state.sell.map((sell) => {
+                    return (
+                        <div key={sell.id}  style = {{border : '1px solid black'}}>
+                            <p>{sell.slug}</p>
+                            <p>{sell.p}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+
+export default Render; 
